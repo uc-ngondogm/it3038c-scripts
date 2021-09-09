d1335d6c368cc0598083a51f843324b5e02c33f2
uc-ngondogm <ngondogm@mail.uc.edu>
2021-09-09 19:23:41 -0400

    powershell files

---
+++ powershell/Ngondogm_lab3.ps1
@@ -0,0 +1,15 @@
+﻿function getIP{ 
+
+    (get-netipaddress).ipv4address | Select-String "192*" 
+} 
+
+$IP = getIP
+$USER = $env:USERNAME
+$NAME = $env:computername
+$VERSION = Get-Host | Select-Object Version
+$DAY = Get-Date | Select-Object Date
+$BODY = "This machine's IP is $IP, user is $USER, Hostname is $NAME, PowerShell version is $VERSION, Today's date is $DATE"
+
+Write-Host($BODY)
+
+Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "guydavidngondo@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
