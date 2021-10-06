16dd30cf913cdf5162a77ee64c104816d989dc6f
uc-ngondogm <ngondogm@mail.uc.edu>
2021-10-06 18:52:51 -0400

    updating nodejs lab

--- node/server.js
+++ node/server.js
@@ -3,11 +3,35 @@ var fs = require("fs");
 var os = require("os");
 var ip = require('ip');
 
+//function getUptime(){
+   //serverTime = os.uptime();
+   //return serverTime
+//}
+ var cpuCount = os.cpus().length
 
-function getUptime(){
-    serverTime = os.uptime();
-    return serverTime
-}
+serverTime = os.uptime();
+var ut_min = serverTime/60;
+var ut_hour = ut_min/60;
+var ut_date = ut_hour/24
+
+serverTime = Math.floor(serverTime);
+ut_min = Math.floor(ut_min);
+ut_hour = Math.floor(ut_hour/24);
+ut_date = Math.floor(ut_date);  
+
+ut_date = ut_date%24
+ut_hour = ut_hour%60;
+ut_min = ut_min%60;
+serverTime = serverTime%60;
+var uptime = ( + ut_date + " Day(s) " + ut_hour + " Hour(s) " + ut_min + " minute(s) and " + serverTime + " second(s)");
+// Get the number of total memory in Byte
+var totalRAM = os.totalmem();
+var total = (totalRAM / (1024 * 1024));
+
+// Get the number of available memory in Byte
+var freeRAM = os.freemem();
+var free = (freeRAM / (1024 * 1024));
+
 var server = http.createServer(function(req, res){
     if (req.url === "/") {
         fs.readFile("./public/index.html", "UTF-8", function(err, body){
@@ -19,20 +43,19 @@ var server = http.createServer(function(req, res){
         myHostName=os.hostname();
         html=`    
         <!DOCTYPE html>
-        <html>
+          <html>
           <head>
-            <title>Node JS Response</title>
-          </head>
-          <body>
-            <p>Hostname: ${myHostName}</p>
-            <p>IP: ${ip.address()}</p>
-            <p>Server Uptime: ${os.uptime}</p>
-            <p>Total Memory: </p>
-            <p>Free Memory: </p>
-            <p>Number of CPUs: </p>            
-          </body>
-        </html>
-    ` 
+              <title>Node JS Response</title>
+              </head>
+              <body>
+              <p>Hostname: ${myHostName}</p>
+              <p>IP: ${ip.address()}</p>
+              <p>Server Uptime: ${uptime}</p>
+              <p>Total Memory: ${total}MB</p>
+              <p>Free Memory: ${free}MB</p>
+              <p>Number of CPUs: ${cpuCount}</p>            
+              </body>
+          </html>` 
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(html);
     }
