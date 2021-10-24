f9754a2406b47f10788ba499bada4a6f668edc3f
uc-ngondogm <ngondogm@mail.uc.edu>
2021-10-24 12:45:04 -0400

    updating script

--- labs/Lab8/lab8.py
+++ labs/Lab8/lab8.py
@@ -5,10 +5,9 @@ page = requests.get(
     "https://webscraper.io/test-sites/e-commerce/allinone")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Create top_items as empty list
+# Create items as empty list
 items = []
-
-# Extract and store in top_items according to instructions on the left
+# Extract and store in items 
 products = soup.select('div.thumbnail')
 for elem in products:
     title = elem.select('h4 > a.title')[0].text
