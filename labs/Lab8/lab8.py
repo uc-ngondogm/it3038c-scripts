5e0818fe6ae2a522b2c56f6a59822e5e894c3157
uc-ngondogm <ngondogm@mail.uc.edu>
2021-10-24 12:45:59 -0400

    Updating script

--- labs/Lab8/lab8.py
+++ labs/Lab8/lab8.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-# Make a request
+
 page = requests.get(
     "https://webscraper.io/test-sites/e-commerce/allinone")
 soup = BeautifulSoup(page.content, 'html.parser')
